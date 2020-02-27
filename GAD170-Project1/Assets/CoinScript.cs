76e545a213ca78bd4aef75e6ef440cb929a53fcb
PropellorHat <61007698+PropellorHat@users.noreply.github.com>
2020-02-27 20:10:08 +0800

    added play and coins

---
+++ GAD170-Project1/Assets/CoinScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    public float spinSpeed = 100f;
+
+    void Update()
+    {
+        this.transform.Rotate(0f, Time.deltaTime * this.spinSpeed, 0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
